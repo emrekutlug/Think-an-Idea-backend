0911a9ba9c80ec8e63f4701287ebe6e8a44fc237
Yigit Polat <yigitpolatt@gmail.com>
2019-08-21 10:05:45 +0300

    minor fix

--- User/userApi.js
+++ User/userApi.js
@@ -8,11 +8,10 @@ router.use('/', (req, res, next) => {
 });
 
 router.post("/addUser", (req, res) => {
-    console.log("UID", users.getUser(req.body.uid));
-
     users.getUser(req.body.uid).then((user) => {
         if (user.length === 0) {
-            users.addUser(req.body)
+            users.addUser(req.body);
+            res.status(200).json({result: `User added with uid ${req.body.uid}`});
         }
         res.status(200).json({user: user});
     }).catch((error) => {
@@ -33,9 +32,7 @@ router.post("/setAdmin",  (req, res) => {
         if (user.length === 0) {
             return res.status(500).json({result:"No such user"});
         }
-        console.log("USER UID: " + req.body.uid);
         user = await users.setAdmin(req.body.uid);
-
         res.status(200).json({user: user});
     }).catch((error) => {
         res.status(500).json(error);
